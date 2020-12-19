ecbf27d4f936653e9b04253ce358d5071ebf5e38
Tejas H <tejas007tez@gmail.com>
2020-12-20 01:32:19 +0530

    Added development mode to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     publicPath: '/',
